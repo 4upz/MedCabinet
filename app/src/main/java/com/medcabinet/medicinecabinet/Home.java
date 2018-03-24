41bcfcf85aabc644afff55f580186cede38a9f34
Dana <dwwigcrew@gmail.com>
2018-03-23 22:30:14 -0700

    Revert "Revert "YKWTFGO""
    This reverts commit f5405839e2c667d20e835deea72a2c8bd215bf0e.

--- app/src/main/java/com/medcabinet/medicinecabinet/Home.java
+++ app/src/main/java/com/medcabinet/medicinecabinet/Home.java
@@ -3,16 +3,15 @@ package com.medcabinet.medicinecabinet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
 
         Button button = findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
