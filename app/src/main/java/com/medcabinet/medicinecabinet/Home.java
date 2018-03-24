0ac965cc27de28b989dfa402d8bba54296c9d37f
jenkins6g <gjenkins@villanova.edu>
2018-03-24 05:27:48 -0400

    Tried out v1 of firebase database

--- app/src/main/java/com/medcabinet/medicinecabinet/Home.java
+++ app/src/main/java/com/medcabinet/medicinecabinet/Home.java
@@ -1,16 +1,22 @@
 package com.medcabinet.medicinecabinet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +28,23 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+        //DatabaseReference myRef = database.getReference("helloworld");
+        DatabaseReference myRef = database.getReference("/gus");
+
+        Task<Void> task = myRef.setValue("i am awesome");
+        task.addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d(TAG, "task complete");
+            }
+        });
+        Log.d(TAG, "trying to save data:" + myRef + " task" + task);
         setContentView(R.layout.home);
+        
 
         Button button = findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +53,6 @@ public class Home extends AppCompatActivity {
                 goToMain2Activity();
             }
         });
-
 // ...
 
 // Choose authentication providers
